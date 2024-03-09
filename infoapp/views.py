1d9ba0110cf75dedac23dffdd6d69619638f2822
Timor Dadgar <nudelvodka@gmail.com>
2024-03-09 09:00:54 +0100

    AWS Database Added
    - Improved File Structure
    - AWS Database Implemented
    - Database Login Credentials Need to be Hidden

--- infoapp/views.py
+++ infoapp/views.py
@@ -1,10 +1,13 @@
+from django.shortcuts import render
 from django.http import HttpResponse
 
 def hello_world(request):
-
     response = HttpResponse(calculate_ur_mom())
     response.write("kg yo mama")
     return response 
 
 def calculate_ur_mom():
     return 10+100
+
+def index(request):
+    return HttpResponse("Hello, world. You're at the polls index.")
